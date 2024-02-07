a5435bdb9f5bb7fd30989326e78497d54a386372
Fabian Vilers <fabian.vilers@dev-one.com>
2024-02-07 08:30:42 +0100

    Add the add command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
-use std::{fmt::Display, fs::File, io::BufReader, path::Path};
+use std::{
+    fmt::Display,
+    fs::{File, OpenOptions},
+    io::{BufReader, BufWriter},
+    path::Path,
+};
 
 #[derive(Parser)]
 #[command(about = "A simple command line to-do manager")]
@@ -12,6 +17,9 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    #[command(about = "Add a task")]
+    Add { task: String },
+
     #[command(about = "List tasks")]
     List {
         #[arg(short, long, help = "Include done tasks")]
@@ -23,6 +31,7 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
+        Some(Commands::Add { task }) => add_task(task),
         Some(Commands::List { all }) => list_tasks(all),
         None => {}
     }
@@ -33,6 +42,16 @@ struct Task {
     id: u32,
     task: String,
     done: bool,
+}
+
+impl Task {
+    fn new(id: u32, task: String) -> Self {
+        Task {
+            id,
+            task,
+            done: false,
+        }
+    }
 }
 
 impl Display for Task {
@@ -53,7 +72,30 @@ fn read_tasks<P: AsRef<Path>>(path: P) -> Result<Vec<Task>> {
     Ok(tasks)
 }
 
+fn write_tasks<P: AsRef<Path>>(path: P, tasks: Vec<Task>) -> Result<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&path)?;
+    let writer = BufWriter::new(file);
+
+    Ok(serde_json::to_writer(writer, &tasks)?)
+}
+
 const DEFAULT_FILENAME: &str = "c:\\Users\\Fabian\\tasks.json";
+
+fn add_task(task: impl Into<String>) {
+    let mut tasks = read_tasks(DEFAULT_FILENAME).unwrap_or(Vec::new());
+    let max_id = tasks.iter().map(|task| task.id).max().unwrap_or(0);
+    let new_task = Task::new(max_id + 1, task.into());
+
+    tasks.push(new_task);
+
+    if write_tasks(DEFAULT_FILENAME, tasks).is_err() {
+        eprintln!("Could not write to {DEFAULT_FILENAME}")
+    }
+}
 
 fn list_tasks(all: bool) {
     let mut tasks = read_tasks(DEFAULT_FILENAME).unwrap_or(Vec::new());
